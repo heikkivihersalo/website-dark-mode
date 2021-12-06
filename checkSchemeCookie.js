a13bd75e0fac945cc7e2e6b12908ee69169cc56f
Heikki Vihersalo <heikki@vihersalo.fi>
2021-12-06 23:20:17 +0200

    Improved comments

--- checkSchemeCookie.js
+++ checkSchemeCookie.js
@@ -1,19 +1,28 @@
 /**
  * Checks preferred color-scheme on page load
- * 
+ *
  * Adds CSS class inside website <html> tag to control preferred color-scheme.
  * Class for light-scheme: 'color-scheme--light'
  * Class for dark-scheme: 'color-scheme--dark'
- * 
+ *
  * Uses color-scheme cookie
- * 
+ *
  */
 
-const cookies = document.cookie.split(';'); // Save current cookies to a variable
+/* Create variables */
+const cookies = document.cookie.split(";");
+const classes = document.getElementsByTagName("html")[0].classList;
 
-// Check the value of color-scheme. If correct cookie is found, add scheme class to <html>
-if (cookies.some((c) => c.includes('color-scheme=dark'))) {
-    document.getElementsByTagName('html')[0].classList.add('color-scheme--dark'); // Dark mode
-} else if (cookies.some((c) => c.includes('color-scheme=light'))) {
-    document.getElementsByTagName('html')[0].classList.add('color-scheme--light'); // Light mode
+/**
+ * Check the value of color-scheme.
+ * If correct cookie is found, add scheme class to <html>
+ */
+
+/* Dark Mode */
+if (cookies.some((c) => c.includes("color-scheme=dark"))) {
+  classes.add("color-scheme--dark");
+
+  /* Light Mode */
+} else if (cookies.some((c) => c.includes("color-scheme=light"))) {
+  classes.add("color-scheme--light");
 }
